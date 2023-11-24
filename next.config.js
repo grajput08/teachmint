a06c49777e12285f70ddd46fa77343ec9913fd6c
Gatik Rajput <grajput2@asu.edu>
2023-11-24 16:06:52 +0530

    update the import url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    urlImports: [
+      "https://jsonplaceholder.typicode.com",
+      "http://worldtimeapi.org/api",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
